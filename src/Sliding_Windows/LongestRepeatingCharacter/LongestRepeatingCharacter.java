ecf6240ec37adfef5a25ada05ef30212f4aae24c
Gurbaj Singh <gurbaj8226@gmail.com>
2025-10-27 21:33:42 -0400

    Completed Longest Repeating Character Replacement and README file

--- src/Sliding_Windows/LongestRepeatingCharacter/LongestRepeatingCharacter.java
+++ src/Sliding_Windows/LongestRepeatingCharacter/LongestRepeatingCharacter.java
@@ -1,5 +1,26 @@
 package Sliding_Windows.LongestRepeatingCharacter;
 
-public class LongestRepeatingCharacter {
+import java.util.HashMap;
+import java.util.Map;
 
+public class LongestRepeatingCharacter {
+	public static int characterReplacement(String s, int k) {
+
+		Map<Character, Integer> map = new HashMap<>();
+		int left = 0;
+		int maxF = 0;
+		int result = 0;
+		for (int right = 0; right < s.length(); right++) {
+			map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+			maxF = Math.max(maxF, map.get(s.charAt(right)));
+			while ((right - left + 1) - maxF > k) {
+				map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+				left++;
+			}
+			result = Math.max(result, right - left + 1);
+		}
+
+		return result;
+
+	}
 }
