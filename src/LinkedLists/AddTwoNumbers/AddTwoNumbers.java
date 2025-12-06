29c07176b0e4265a5feebf5d51947e50f160b60a
Gurbaj Singh <gurbaj8226@gmail.com>
2025-12-05 21:45:51 -0500

    Completed Add Two Numbers

---
+++ src/LinkedLists/AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,36 @@
+package LinkedLists.AddTwoNumbers;
+
+public class AddTwoNumbers {
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode node = dummy;
+
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0){
+
+            int v1 = (l1 != null ) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+
+            int val = v1 + v2 + carry;
+
+            carry = val / 10;
+            val = val % 10;
+
+            node.next = new ListNode(val);
+            node = node.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        return dummy.next;
+    }
+}
+class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
